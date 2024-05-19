88d14f90ff2d7ccd23e3982cc02a2f1b8e2ee4c9
mikabrytu <mikabrytu@gmail.com>
2024-05-19 14:43:07 -0300

    Move player by input

--- scenes/nodes/player/player.gd
+++ scenes/nodes/player/player.gd
@@ -1,8 +1,26 @@
 extends Node2D
 
 @export var speed: float
-@export var direction: Vector2i
 
+var direction: Vector2i
+
+
+func _ready():
+	direction = Vector2i.RIGHT
+	
 
 func _process(delta):
+	_get_direction()
 	position += direction * speed
+	
+
+func _get_direction():
+	if (Input.is_action_just_pressed("action_up")):
+		direction = Vector2i.UP
+	if (Input.is_action_just_pressed("action_down")):
+		direction = Vector2i.DOWN
+	if (Input.is_action_just_pressed("action_left")):
+		direction = Vector2i.LEFT
+	if (Input.is_action_just_pressed("action_right")):
+		direction = Vector2i.RIGHT
+	

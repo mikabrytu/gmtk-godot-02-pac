df4df18dd809216ac85dcbcedc45040419877880
mikabrytu <mikabrytu@gmail.com>
2024-05-19 15:24:50 -0300

    wip - Detect when player can move

--- scenes/nodes/player/player.gd
+++ scenes/nodes/player/player.gd
@@ -1,6 +1,8 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed: float
+
+@onready var ray_group = $"Ray Group"
 
 var direction: Vector2i
 
@@ -10,17 +12,40 @@ func _ready():
 	
 
 func _process(delta):
-	_get_direction()
-	position += direction * speed
+	if (_can_change_direction()):
+		velocity = direction * speed
 	
 
-func _get_direction():
-	if (Input.is_action_just_pressed("action_up")):
-		direction = Vector2i.UP
-	if (Input.is_action_just_pressed("action_down")):
-		direction = Vector2i.DOWN
-	if (Input.is_action_just_pressed("action_left")):
-		direction = Vector2i.LEFT
-	if (Input.is_action_just_pressed("action_right")):
-		direction = Vector2i.RIGHT
+func _physics_process(delta):
+	move_and_slide()
 	
+
+func _can_change_direction() -> bool:
+	var d = direction
+	
+	if (Input.is_action_just_pressed("action_up")):
+		ray_group.rotation_degrees = -90
+		d = Vector2i.UP
+	
+	if (Input.is_action_just_pressed("action_down")):
+		ray_group.rotation_degrees = 90
+		d = Vector2i.DOWN
+	
+	if (Input.is_action_just_pressed("action_left")):
+		ray_group.rotation_degrees = 180
+		d = Vector2i.LEFT
+	
+	if (Input.is_action_just_pressed("action_right")):
+		ray_group.rotation_degrees = 0
+		d = Vector2i.RIGHT
+	
+	if (ray_group.get_node("Up Ray").is_colliding()):
+		print("Up ray is colliding | Group Ray Rotation: " + str(ray_group.rotation) + " | Real direction: " + str(direction))
+		return false
+	
+	if (ray_group.get_node("Bottom Ray").is_colliding()):
+		print("Bottom ray is colliding | Cached Direction: " + str(d) + " | Real direction: " + str(direction))
+		return false
+	
+	direction = d
+	return true

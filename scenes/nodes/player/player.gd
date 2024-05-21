b9e5013e19fdf2900c3331729d0c0afc2aacf3eb
mikabrytu <mikabrytu@gmail.com>
2024-05-21 03:38:05 -0300

    Detect new path and change to it

--- scenes/nodes/player/player.gd
+++ scenes/nodes/player/player.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 @export var speed: float
-@export var server_path: PathFollow2D
+@export var initial_path: PathFollow2D
+
+@onready var ray = $RayCast2D as RayCast2D
 
 var direction: Vector2i
 var path: PathFollow2D
@@ -11,34 +13,45 @@ var path: PathFollow2D
 func _ready():
 	direction = Vector2i.RIGHT
 	
-	_set_path()
+	_set_path(initial_path)
 	path.progress_ratio = 0.5
-	reparent.call_deferred(path)
 	
 
-func _process(delta):
-	path.progress += _get_direction().x * speed * delta
+func _physics_process(delta):
+	var direction = _get_direction()
+	var axis = direction.x if direction.x != 0 else direction.y
+	path.progress += axis * speed * delta
 	
 
 #endregion
 
 #region Implementation
 
-func _set_path():
-	path = server_path
+func _set_path(new_path):
+	if (new_path != null):
+		path = new_path
+		reparent.call_deferred(path)
 	
 
 func _get_direction() -> Vector2i:
 	if (Input.is_action_just_pressed("action_up")):
-		direction = Vector2i.UP
-	if (Input.is_action_just_pressed("action_down")):
 		direction = Vector2i.DOWN
+	
+	if (Input.is_action_just_pressed("action_down")):
+		_find_new_path()
+		direction = Vector2i.UP
+	
 	if (Input.is_action_just_pressed("action_left")):
 		direction = Vector2i.LEFT
+	
 	if (Input.is_action_just_pressed("action_right")):
 		direction = Vector2i.RIGHT
 	
 	return direction
 	
+
+func _find_new_path():
+	if (ray.is_colliding()):
+		_set_path(ray.get_collider().get_parent())
 
 #endregion

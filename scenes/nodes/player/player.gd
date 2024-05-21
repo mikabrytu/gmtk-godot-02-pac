cdf98a7d4757d2ae9a962137d488ad569dedfb57
mikabrytu <mikabrytu@gmail.com>
2024-05-21 02:58:14 -0300

    Player moves along path

--- scenes/nodes/player/player.gd
+++ scenes/nodes/player/player.gd
@@ -1,22 +1,32 @@
 extends Area2D
 
 @export var speed: float
+@export var server_path: PathFollow2D
 
 var direction: Vector2i
+var path: PathFollow2D
 
 #region Godot Messages
 
 func _ready():
 	direction = Vector2i.RIGHT
 	
+	_set_path()
+	path.progress_ratio = 0.5
+	reparent.call_deferred(path)
+	
 
 func _process(delta):
-	position += _get_direction() * speed * delta
+	path.progress += _get_direction().x * speed * delta
 	
 
 #endregion
 
 #region Implementation
+
+func _set_path():
+	path = server_path
+	
 
 func _get_direction() -> Vector2i:
 	if (Input.is_action_just_pressed("action_up")):

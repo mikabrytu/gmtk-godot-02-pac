af2094b3cf8c01efc867d5b4e3b7611094f13812
mikabrytu <mikabrytu@gmail.com>
2024-05-27 20:49:42 -0300

    Improve movement and make it smooth with lerp

--- scenes/nodes/player/player.gd
+++ scenes/nodes/player/player.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
+@export var speed: float
+
 var board: TileMap
 var direction: Vector2i
+var previous_position: Vector2i
+var target_position: Vector2i
+var time: float
 var can_move = true
+var is_moving = false
 
 
 #region Godot Messages
@@ -14,7 +20,12 @@ func _ready():
 
 func _process(delta):
 	_check_direction()
-	_move()
+	_get_next_position()
+	_check_distance_to_target()
+	
+
+func _physics_process(delta):
+	_move(delta)
 	
 
 #endregion
@@ -32,7 +43,9 @@ func _check_direction():
 		direction = Vector2i.RIGHT
 	
 
-func _move():
+func _get_next_position():
+	if (is_moving): return
+	
 	var current_tile = board.local_to_map(global_position)
 	var target_tile = current_tile + direction
 	var tile_data = board.get_cell_tile_data(0, target_tile)
@@ -40,7 +53,24 @@ func _move():
 	can_move = tile_data.get_custom_data("Walkable")
 	
 	if (can_move):
-		global_position = board.map_to_local(target_tile)
+		previous_position = global_position
+		target_position = board.map_to_local(target_tile)
+		is_moving = true
+	else:
+		is_moving = false
+	
+
+func _move(delta):
+	if (!is_moving): return
+	
+	time += delta * speed
+	global_position = Vector2(previous_position).lerp(target_position, time)
+	
+
+func _check_distance_to_target():
+	if (Vector2i(global_position) == target_position):
+		is_moving = false
+		time = 0
 	
 
 #endregion

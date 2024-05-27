0173cf0fd9229bf815466d5c94679dd934adc4db
mikabrytu <mikabrytu@gmail.com>
2024-05-27 14:49:38 -0300

    Set walkable tiles on board and allow player movement only in them

--- scenes/nodes/player/player.gd
+++ scenes/nodes/player/player.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 var board: TileMap
 var direction: Vector2i
+var can_move = true
 
 
 #region Godot Messages
 
 func _ready():
 	board = NodeProvider.get_board()
+	direction = Vector2i.RIGHT
 	
 
 func _process(delta):
@@ -33,8 +35,12 @@ func _check_direction():
 func _move():
 	var current_tile = board.local_to_map(global_position)
 	var target_tile = current_tile + direction
+	var tile_data = board.get_cell_tile_data(0, target_tile)
 	
-	print(str(current_tile) + " | " + str(target_tile))
+	can_move = tile_data.get_custom_data("Walkable")
+	
+	if (can_move):
+		global_position = board.map_to_local(target_tile)
 	
 
 #endregion

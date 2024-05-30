1535ca1ea3c8661e50d6e6c07aab8bb500ca362d
mikabrytu <mikabrytu@gmail.com>
2024-05-30 10:05:58 -0300

    Extract movement logic to a system node

---
+++ scenes/nodes/grid_movement_2D/grid_movement_2d.gd
@@ -0,0 +1,61 @@
+class_name GridMovement2D extends Node2D
+
+@export var speed: float
+
+var board: TileMap
+var parent: Node2D
+var previous_position: Vector2i
+var target_position: Vector2i
+var time: float
+var can_move = true
+var is_moving = false
+
+#region Godot Messages
+
+func _ready():
+	board = NodeProvider.get_board()
+	parent = get_parent()
+	
+
+func _process(delta):
+	_check_distance_to_target()
+	
+
+#endregion
+
+#region Implementation
+
+func _check_distance_to_target():
+	if (Vector2i(parent.global_position) == target_position):
+		is_moving = false
+		time = 0
+	
+
+#endregion
+
+#region Public API
+
+func find_next_position(direction: Vector2i):
+	if (is_moving): return
+	
+	var current_tile = board.local_to_map(parent.global_position)
+	var target_tile = current_tile + direction
+	var tile_data = board.get_cell_tile_data(0, target_tile)
+	var can_move = tile_data.get_custom_data("Walkable")
+	
+	if (can_move):
+		previous_position = parent.global_position
+		target_position = board.map_to_local(target_tile)
+		is_moving = true
+	else:
+		is_moving = false
+	
+
+func move(delta):
+	if (!is_moving): return
+	
+	time += delta * speed
+	parent.global_position = Vector2(previous_position).lerp(target_position, time)
+	
+
+#endregion
--- scenes/nodes/player/player.gd
+++ scenes/nodes/player/player.gd
@@ -1,38 +1,30 @@
 extends Node2D
 
-@export var speed: float
+@onready var grid_movement: GridMovement2D = $GridMovement2D
 
-var board: TileMap
 var direction: Vector2i
-var previous_position: Vector2i
-var target_position: Vector2i
-var time: float
-var can_move = true
-var is_moving = false
 
 
 #region Godot Messages
 
 func _ready():
-	board = NodeProvider.get_board()
 	direction = Vector2i.RIGHT
 	
 
 func _process(delta):
-	_check_direction()
-	_get_next_position()
-	_check_distance_to_target()
+	_get_direction()
+	grid_movement.find_next_position(direction)
 	
 
 func _physics_process(delta):
-	_move(delta)
+	grid_movement.move(delta)
 	
 
 #endregion
 
 #region Implementation
 
-func _check_direction():
+func _get_direction():
 	if (Input.is_action_just_pressed("up")):
 		direction = Vector2i.UP
 	if (Input.is_action_just_pressed("down")):
@@ -41,36 +33,6 @@ func _check_direction():
 		direction = Vector2i.LEFT
 	if (Input.is_action_just_pressed("right")):
 		direction = Vector2i.RIGHT
-	
-
-func _get_next_position():
-	if (is_moving): return
-	
-	var current_tile = board.local_to_map(global_position)
-	var target_tile = current_tile + direction
-	var tile_data = board.get_cell_tile_data(0, target_tile)
-	
-	can_move = tile_data.get_custom_data("Walkable")
-	
-	if (can_move):
-		previous_position = global_position
-		target_position = board.map_to_local(target_tile)
-		is_moving = true
-	else:
-		is_moving = false
-	
-
-func _move(delta):
-	if (!is_moving): return
-	
-	time += delta * speed
-	global_position = Vector2(previous_position).lerp(target_position, time)
-	
-
-func _check_distance_to_target():
-	if (Vector2i(global_position) == target_position):
-		is_moving = false
-		time = 0
 	
 
 #endregion
